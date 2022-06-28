bc9c54d60842876ac8d7519cb98aa47dc4becba8
littledev13 <irvandn13@gmail.com>
2022-06-28 20:28:18 +0700

    Stater Pack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+ReactDOM.render(
+  <h1 className="text-orange-400 font-5xl">Hello Viewer</h1>,
+  root
+);
